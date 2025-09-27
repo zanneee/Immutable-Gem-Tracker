775888e57e7bae290fa7dbd597071c9832817723
Jinsung Kim <zanneee@gmail.com>
2025-09-27 13:08:35 +0900

    fix: correct import path for types in API route

--- pages/api/gems/[wallet].ts
+++ pages/api/gems/[wallet].ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import type { GemEarning, GemResponse, GemStats } from '../../types';
+import type { GemEarning, GemResponse, GemStats } from '../../../types';
 
 export default async function handler(
     req: NextApiRequest,
